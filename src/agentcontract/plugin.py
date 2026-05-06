4ec3e88829997fb17c555a790162320c073a4f4b
mikiships <frank.michaels2123@gmail.com>
2026-05-06 03:04:25 -0400

    Remove confirmed unused internal arguments
    Nightshift-Task: dead-code
    
    Nightshift-Ref: https://github.com/marcus/nightshift

--- src/agentcontract/plugin.py
+++ src/agentcontract/plugin.py
@@ -147,7 +147,7 @@ def ac_replay_engine(
 
 
 @pytest.fixture
-def ac_assert(ac_config: AgentContractConfig) -> AssertionEngine:
+def ac_assert() -> AssertionEngine:
     """Provide an AssertionEngine instance."""
     return AssertionEngine()
 
